88f8746a99aeedb1ed963ba8db1b0cb4dad63a91
Ahanaf Abrar <100956005+Ahanaf-Abrar-Trishad@users.noreply.github.com>
2023-11-13 22:13:47 +0600

    Add files via upload

--- LAB04/Task 06/task6_don't work for 7.py
+++ LAB04/Task 06/task6_don't work for 7.py
@@ -1,43 +1,35 @@
 ipt_file = open("input6_7.txt", "r")
-opt_file = open("output6_7.txt", "w")
+opt_file= open("output6_7.txt", "w")
 R, H = ipt_file.readline().strip().split(' ')
 grid = []
 for i in range(int(R)):
     line = ipt_file.readline().strip('\n')
     grid.append(line)
-# print(grid)
-
 
 def DFS(grid, visited, i, j):
-    if (i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or visited[i][j] or grid[i][j] == "#"):
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or visited[i][j] or grid[i][j] == '#':
         return 0
-
+    
     visited[i][j] = True
     diamonds = 0
 
-    if grid[i][j] == "D":
+    if grid[i][j] == 'D':
         diamonds += 1
-
-    diamonds += (
-        DFS(grid, visited, i + 1, j)
-        + DFS(grid, visited, i - 1, j)
-        + DFS(grid, visited, i, j + 1)
-        + DFS(grid, visited, i, j - 1)
-    )
+    diamonds += max(DFS(grid, visited, i+1, j), DFS(grid, visited, i-1, j), DFS(grid, visited, i, j+1), DFS(grid, visited, i, j-1))
+    visited[i][j] = False
 
     return diamonds
 
-
-visited = [[False] * int(H) for i in range(int(R))]
+visited = [[False]*int(H) for i in range(int(R))]
 maxDiamond = 0
 for i in range(int(R)):
     for j in range(int(H)):
-        if grid[i][j] == ".":
+        if grid[i][j] == '.':
             diamonds = DFS(grid, visited, i, j)
             maxDiamond = max(maxDiamond, diamonds)
-
 opt_file.write(str(maxDiamond))
 print(maxDiamond)
+
 
 ipt_file.close()
 opt_file.close()
